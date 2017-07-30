2677b0572e3b5d5d4650e1d519df44ca3a20b5b0
lynner123 <shandonglyn@163.com>
2017-07-30 13:57:34 +0000

    finish

--- main/section-1/practice-1.js
+++ main/section-1/practice-1.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var con =[];
+          for(var i=0; i<collectionA.length; i++)
+          {
+                    for(var j=0; j<collectionB.length; j++)
+                    {
+                              if(collectionA[i]==collectionB[j])
+                              {
+                                    con.push(collectionA[i]);
+                              }
+                    }
+          }
+  return con;//'实现练习要求，并改写该行代码。
 }
