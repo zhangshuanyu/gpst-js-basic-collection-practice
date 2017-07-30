317af045b83841c9919ffaead72ce5ad6763229d
lynner123 <shandonglyn@163.com>
2017-07-30 18:26:12 +0000

    finish

--- main/section-2/practice-1.js
+++ main/section-2/practice-1.js
@@ -1,5 +1,30 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+          var con =[];
+          var indices = [];
+          var idx = -1;
+          var k=[];
+          var cnt=[];
+          for(var i=0; i<collection.length; i++)
+          {
+                   if(k.includes(collection[i])){
+                              continue;
+                   }else{
+                              k.push(collection[i]);
+                              idx = collection.indexOf(collection[i]);
+                              while (idx != -1) {
+                                        indices.push(idx);
+                                        idx = collection.indexOf(collection[i], idx + 1);
+                              }
+                              cnt.push(indices.length);
+                   }
+                   indices = [];
+          }
+          for(var l=0;l<k.length;l++)
+          {
+                    con.push({key:k[l],count:cnt[l]});
+          }
+
+  return  con; //  '实现练习要求，并改写该行代码。';
 }
--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,5 +1,40 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+          var con =[];
+          var indices = [];
+          var idx = -1;
+          var k=[];
+          var cnt=[];
+          for(var i=0; i<collection.length; i++)
+          {
+                   if(k.includes(collection[i])){
+                              continue;
+                   }else{
+                             if(collection[i].length==1){
+                                        idx = collection.indexOf(collection[i]);
+                                        while (idx != -1) {
+                                                  indices.push(idx);
+                                                  idx = collection.indexOf(collection[i], idx + 1);
+                                        }                              
+                                        k.push(collection[i]);
+                                        cnt.push(indices.length);
+                              }else{
+                                        if(k.includes(collection[i][0])){//             /[1-9]\d*/             /\d{1,2}/
+                                              cnt[k.indexOf(collection[i][0])] += parseInt(collection[i].match(/[1-9]\d*/));
+                                        }else{
+                                                  k.push(collection[i][0]);
+                                                  cnt.push(parseInt(collection[i].match(/\d/)));
+                                        }                                       
+                              }
+                              
+                   }
+                   indices = [];
+          }
+          for(var l=0;l<k.length;l++)
+          {
+                    con.push({name:k[l],summary:cnt[l]});
+          }
+
+  return  con; //  '实现练习要求，并改写该行代码。';
 }
